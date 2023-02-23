9829fe6dd879dc6f08a893721dd97b365bff00e6
dredegui <guilherme.m.pascoal@tecnico.ulisboa.pt>
2023-02-23 19:02:38 +0000

    add: media containers php

--- MoviesPage.php
+++ MoviesPage.php
@@ -15,7 +15,7 @@
 </head>
 
 <body>
-    <?php echo createNavBar("Movies") ?>
+    <?php echo createNavBar("Movies");?>
 
     <div style="background-color: transparent;">
         <form class="d-flex " role="search ">
@@ -24,7 +24,54 @@
         </form>
     </div>
     <div class="movie-container">
+        <?php
+            // Obter lista de filmes
+            $stmt = $pdo->query('SELECT * FROM media WHERE tipo IN (0, 2)');
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            // Iterar sobre os filmes
+            foreach ($results as $result) {
+                // Estrutura div para um filme
+                $html = '<div class="container">
+                <h3 class="titulo">' . $result["titulo"] . '</h3>
+                <img src= ' . '"' . $result['caminho'] . '">
+                <p class="descricao">' . $result["descricao"] . '</p>';
+                // Verificar se sessão esta ativa
+                if (isset($_SESSION["id"])) {
+                    // Se sim tem-se checkbox
+                    $html = $html . '<form method="POST">'; 
+                    $stmt = $pdo->prepare('SELECT * FROM vistos WHERE titulo = ? AND id = ?');
+                    $stmt->execute([$result["titulo"], $_SESSION["id"]]);
+                    if ($stmt->rowCount() != 0) {
+                        // Se o utilizador ja viu a checkbox esta ativa
+                        $html = $html .
+                        '<input type="checkbox" class="check" name="visto" checked >Visto</input>';
+                    } else {
+                        $html = $html .
+                        '<input type="checkbox" class="check" name="visto" >Visto</input>';
+                    }
+                    $stmt = $pdo->prepare('SELECT * FROM para_ver WHERE titulo = ? AND id = ?');
+                    $stmt->execute([$result["titulo"], $_SESSION["id"]]);
+                    if ($stmt->rowCount() != 0) {
+                        // Se o utilizador ja registrou na lista para ver a checkbox esta ativa
+                        $html = $html .
+                        '<input type="checkbox" class="check" name="pver" checked>Para ver</input>';
+                    } else {
+                        $html = $html .
+                        '<input type="checkbox" class="check"  name="pver" >Para ver</input>';
+                    }
+                    $html = $html . '<input type="submit" name="submit">'; 
+                    $html = $html . '</form>'; 
+                }
+                $html = $html . '</div>';
+                echo $html;
+                /*if (isset($_POST["visto"])) {
 
+                }
+                if (isset($_POST["visto"])) {
+
+                }*/
+            }
+        ?>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js " integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN " crossorigin="anonymous "></script>
 </body>
